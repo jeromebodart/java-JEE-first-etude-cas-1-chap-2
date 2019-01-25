bb826cb848126e33cf478934de19672077affe28
jeromebodart <jerome.bodart@hei.yncrea.fr>
2019-01-25 09:50:39 +0100

    Message de validation

--- elections-01/src/istia/st/elections/MainElections.java
+++ elections-01/src/istia/st/elections/MainElections.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainElections {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	
+	/*commmentaires*/
 		boolean saisieOk = false;
 		int nbSiegesAPourvoir;
 		int nbListes;
